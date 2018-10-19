b3e2b4a1d5ff49884f7b67310785865e3456e99f
rohithkrn <rohith.nallamaddi@gmail.com>
2018-10-19 16:52:32 -0700

    remove roi_align _rotated_op_test from ignored list

--- .jenkins/caffe2/amd/python_tests.sh
+++ .jenkins/caffe2/amd/python_tests.sh
@@ -18,9 +18,6 @@ rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/operator_test/unique_ops_test
 rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/model_device_test.py")
 rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/data_parallel_model_test.py")
 
-# Need to go through roi ops to replace max(...) with fmaxf(...)
-rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/operator_test/roi_align_rotated_op_test.py")
-
 # cuda top_k op has some asm code, the hipified version doesn't
 # compile yet, so we don't have top_k operator for now
 rocm_ignore_test+=("--ignore $CAFFE2_PYPATH/python/operator_test/top_k_test.py")
