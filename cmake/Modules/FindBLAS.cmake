ce15313366eb2272e9fe35e300855f8bb0a12a45
Shruti <shruti.ramesh@amd.com>
2020-01-13 15:32:52 +0530

    Adding PyTorch+MKLDNN+BLIS path

--- cmake/Modules/FindBLAS.cmake
+++ cmake/Modules/FindBLAS.cmake
@@ -20,7 +20,7 @@ SET(BLAS_INCLUDE_DIR)
 SET(BLAS_INFO)
 SET(BLAS_F2C)
 
-SET(WITH_BLAS "" CACHE STRING "Blas type [mkl/open/goto/acml/atlas/accelerate/veclib/generic]")
+SET(WITH_BLAS "" CACHE STRING "Blas type [blis/mkl/open/goto/acml/atlas/accelerate/veclib/generic]")
 
 # Old FindBlas
 INCLUDE(CheckCSourceRuns)
@@ -62,12 +62,12 @@ MACRO(Check_Fortran_Libraries LIBRARIES _prefix _name _flags _list)
       if ( APPLE )
         find_library(${_prefix}_${_library}_LIBRARY
           NAMES ${_library}
-          PATHS /usr/local/lib /usr/lib /usr/local/lib64 /usr/lib64 /opt/OpenBLAS/lib /usr/lib/aarch64-linux-gnu
+	  PATHS /usr/local/lib /usr/lib /usr/local/lib64 /usr/lib64 /opt/OpenBLAS/lib $HOME/amd-blis-mt/lib /usr/lib/aarch64-linux-gnu
           ENV DYLD_LIBRARY_PATH )
       else ( APPLE )
         find_library(${_prefix}_${_library}_LIBRARY
           NAMES ${_library}
-          PATHS /usr/local/lib /usr/lib /usr/local/lib64 /usr/lib64 /opt/OpenBLAS/lib /usr/lib/aarch64-linux-gnu
+          PATHS /usr/local/lib /usr/lib /usr/local/lib64 /usr/lib64 /opt/OpenBLAS/lib $HOME/amd-blis-mt/lib /usr/lib/aarch64-linux-gnu
           ENV LD_LIBRARY_PATH )
       endif( APPLE )
       mark_as_advanced(${_prefix}_${_library}_LIBRARY)
@@ -103,6 +103,20 @@ if((NOT BLAS_LIBRARIES)
     SET(BLAS_INCLUDE_DIR ${MKL_INCLUDE_DIR})
     SET(BLAS_VERSION ${MKL_VERSION})
   ENDIF(MKL_FOUND)
+endif()
+
+#BLIS?
+if((NOT BLAS_LIBRARIES)
+    AND ((NOT WITH_BLAS) OR (WITH_BLAS STREQUAL "blis")))
+  check_fortran_libraries(
+  BLAS_LIBRARIES
+  BLAS
+  sgemm
+  ""
+  "blis")
+  if(BLAS_LIBRARIES)
+    set(BLAS_INFO "blis")
+  endif(BLAS_LIBRARIES)
 endif()
 
 # Apple BLAS library?
@@ -147,6 +161,19 @@ if((NOT BLAS_LIBRARIES)
 endif()
 
 if((NOT BLAS_LIBRARIES)
+    AND ((NOT WITH_BLAS) OR (WITH_BLAS STREQUAL "blis")))
+  check_fortran_libraries(
+  BLAS_LIBRARIES
+  BLAS
+  sgemm
+  ""
+  "blis;pthread")
+  if(BLAS_LIBRARIES)
+    set(BLAS_INFO "blis")
+  endif(BLAS_LIBRARIES)
+endif()
+
+if((NOT BLAS_LIBRARIES)
     AND ((NOT WITH_BLAS) OR (WITH_BLAS STREQUAL "open")))
   check_fortran_libraries(
   BLAS_LIBRARIES
@@ -169,6 +196,19 @@ if((NOT BLAS_LIBRARIES) AND (WIN32)
   "libopenblas")
   if(BLAS_LIBRARIES)
     set(BLAS_INFO "open")
+  endif(BLAS_LIBRARIES)
+endif()
+
+if((NOT BLAS_LIBRARIES) AND (WIN32)
+    AND ((NOT WITH_BLAS) OR (WITH_BLAS STREQUAL "blis")))
+  check_fortran_libraries(
+  BLAS_LIBRARIES
+  BLAS
+  sgemm
+  ""
+  "blis")
+  if(BLAS_LIBRARIES)
+    set(BLAS_INFO "blis")
   endif(BLAS_LIBRARIES)
 endif()
 
---
+++ cmake/Modules/Findblis.cmake
@@ -0,0 +1,42 @@
+SET(BLIS_INCLUDE_SEARCH_PATHS
+	~/amd-blis-mt/include/blis
+)
+
+SET(BLIS_LIB_SEARCH_PATHS
+	~/amd-blis-mt/lib
+)
+
+FIND_PATH(BLIS_INCLUDE_DIR NAMES cblas.h blis.h PATHS ${BLIS_INCLUDE_SEARCH_PATHS})
+FIND_LIBRARY(BLIS_LIB NAMES libblis.so.2 PATHS ${BLIS_LIB_SEARCH_PATHS})
+
+SET(BLIS_FOUND ON)
+
+#    Check include files
+IF(NOT BLIS_INCLUDE_DIR)
+        SET(BLIS_FOUND OFF)
+        MESSAGE(STATUS "Could not find BLIS include. Turning BLIS_FOUND off")
+ENDIF()
+
+#    Check libraries
+IF(NOT BLIS_LIB)
+        SET(BLIS_FOUND OFF)
+        MESSAGE(STATUS "Could not find BLIS lib. Turning BLIS_FOUND off")
+ENDIF()
+
+IF (BLIS_FOUND)
+        IF (NOT BLAS_FIND_QUIETLY)
+                MESSAGE(STATUS "Found BLIS libraries: ${BLIS_LIB}")
+                MESSAGE(STATUS "Found BLIS include: ${BLIS_INCLUDE_DIR}")
+        ENDIF (NOT BLAS_FIND_QUIETLY)
+ELSE (BLIS_FOUND)
+        IF (BLAS_FIND_REQUIRED)
+                MESSAGE(FATAL_ERROR "Could not find BLIS")
+        ENDIF (BLAS_FIND_REQUIRED)
+ENDIF (BLIS_FOUND)
+
+MARK_AS_ADVANCED(
+        BLIS_INCLUDE_DIR
+        BLIS_LIB
+        blis
+)
+
