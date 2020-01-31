e8394f0d3219bf022319f39ae5e53453ec08540f
Shruti <shruti.ramesh@amd.com>
2020-01-31 15:01:35 +0530

    Made corrections to Findblis.cmake as per review comments

--- cmake/Modules/Findblis.cmake
+++ cmake/Modules/Findblis.cmake
@@ -1,13 +1,36 @@
 SET(BLIS_INCLUDE_SEARCH_PATHS
-       /usr/local/include/blis
+ /usr/include
+ /usr/include/blis
+ /usr/local/include 
+ /usr/local/include/blis
+ /usr/local/opt/blis/include
+ /opt/blis/include
+ $ENV{BLIS_HOME}
+ $ENV{BLIS_HOME}/include
+
 )
 
 SET(BLIS_LIB_SEARCH_PATHS
-       /usr/local/lib
+ /lib
+ /lib/blis
+ /lib64
+ /lib64/blis
+ /usr/lib
+ /usr/lib/blis
+ /usr/lib64
+ /usr/lib64/blis
+ /usr/local/lib
+ /usr/local/lib64
+ /usr/local/opt/blis/lib
+ /opt/blis/lib
+ $ENV{BLIS}
+ $ENV{BLIS}/lib
+ $ENV{BLIS_HOME}
+ $ENV{BLIS_HOME}/lib
 )
 
 FIND_PATH(BLIS_INCLUDE_DIR NAMES cblas.h blis.h PATHS ${BLIS_INCLUDE_SEARCH_PATHS})
-FIND_LIBRARY(BLIS_LIB NAMES libblis.so PATHS ${BLIS_LIB_SEARCH_PATHS})
+FIND_LIBRARY(BLIS_LIB NAMES libblis PATHS ${BLIS_LIB_SEARCH_PATHS})
 
 SET(BLIS_FOUND ON)
 
@@ -24,14 +47,14 @@ IF(NOT BLIS_LIB)
 ENDIF()
 
 IF (BLIS_FOUND)
-        IF (NOT BLAS_FIND_QUIETLY)
+        IF (NOT BLIS_FIND_QUIETLY)
                 MESSAGE(STATUS "Found BLIS libraries: ${BLIS_LIB}")
                 MESSAGE(STATUS "Found BLIS include: ${BLIS_INCLUDE_DIR}")
-        ENDIF (NOT BLAS_FIND_QUIETLY)
+        ENDIF (NOT BLIS_FIND_QUIETLY)
 ELSE (BLIS_FOUND)
-        IF (BLAS_FIND_REQUIRED)
+        IF (BLIS_FIND_REQUIRED)
                 MESSAGE(FATAL_ERROR "Could not find BLIS")
-        ENDIF (BLAS_FIND_REQUIRED)
+        ENDIF (BLIS_FIND_REQUIRED)
 ENDIF (BLIS_FOUND)
 
 MARK_AS_ADVANCED(
