00952e959021b6feabb83f5cfdd8e501c98b69d2
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2021-10-14 18:46:14 +0000

    Fix the API as per latest library version.
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- aten/src/ATen/cuda/cub.cuh
+++ aten/src/ATen/cuda/cub.cuh
@@ -231,21 +231,20 @@ struct chained_iterator {
 
 template<typename InputIteratorT, typename OutputIteratorT, typename ScanOpT>
 inline void inclusive_scan(InputIteratorT input, OutputIteratorT output, ScanOpT scan_op, int64_t num_items) {
-  // non synchronizing cub call
-  // even though cub is supposed to support tensors with int_max elements, in reality it doesn't,
-  // so split at int_max/2
-  constexpr int max_cub_size = std::numeric_limits<int>::max() / 2 + 1; // 2**30
 #if defined(USE_ROCM) && (ROCM_VERSION >= 50000)
+  //For ROCm, use hipCUB chained iterators
   CUB_WRAPPER(NO_ROCM(detail)::hipcub::DeviceScan::InclusiveScan,
       input,
       output,
       scan_op,
       num_items,
-      at::hip::getCurrentHIPStreamMasqueradingAsCUDA(),
-      false,
-      max_cub_size);
+      at::cuda::getCurrentCUDAStream());
   C10_HIP_KERNEL_LAUNCH_CHECK();
 #else
+  // non synchronizing cub call
+  // even though cub is supposed to support tensors with int_max elements, in reality it doesn't,
+  // so split at int_max/2
+  constexpr int max_cub_size = std::numeric_limits<int>::max() / 2 + 1; // 2**30
   int size_cub = std::min<int64_t>(num_items, max_cub_size);
   CUB_WRAPPER(NO_ROCM(detail)::cub::DeviceScan::InclusiveScan,
       input,
@@ -290,22 +289,21 @@ inline void inclusive_scan(InputIteratorT input, OutputIteratorT output, ScanOpT
 
 template<typename InputIteratorT, typename OutputIteratorT, typename ScanOpT, typename InitValueT>
 inline void exclusive_scan(InputIteratorT input, OutputIteratorT output, ScanOpT scan_op, InitValueT init_value, int64_t num_items) {
-  // non synchronizing cub call
-  // even though cub is supposed to support tensors with int_max elements, in reality it doesn't,
-  // so split at int_max/2
-  constexpr int max_cub_size = std::numeric_limits<int>::max() / 2 + 1; // 2**30
 #if defined(USE_ROCM) && (ROCM_VERSION >= 50000)
+  //For ROCm, use hipCUB chained iterators
   CUB_WRAPPER(NO_ROCM(detail)::hipcub::DeviceScan::ExclusiveScan,
       input,
       output,
       scan_op,
       init_value,
       num_items,
-      at::hip::getCurrentHIPStreamMasqueradingAsCUDA(),
-      false,
-      max_cub_size);
+      at::cuda::getCurrentCUDAStream());
   C10_HIP_KERNEL_LAUNCH_CHECK();
 #else
+  // non synchronizing cub call
+  // even though cub is supposed to support tensors with int_max elements, in reality it doesn't,
+  // so split at int_max/2
+  constexpr int max_cub_size = std::numeric_limits<int>::max() / 2 + 1; // 2**30
   int size_cub = std::min<int64_t>(num_items, max_cub_size);
   CUB_WRAPPER(NO_ROCM(detail)::cub::DeviceScan::ExclusiveScan,
       input,
