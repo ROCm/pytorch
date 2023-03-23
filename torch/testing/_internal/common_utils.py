fe30a2027c1eb633f5b06660a05e55c12764dffd
Pruthvi Madugundu <pruthvigithub@gmail.com>
2023-03-22 23:42:41 -0700

    CONSOLIDATED COMMITS: unit test skips and unskips
    =================================================
    
    Temporarily skip test_conv3d_64bit_indexing
    
    - Rocblas API support is requested
    - SWDEV-383635 & sub task - SWDEV-390218
    
    Skip ddp apply_optim_in_bwd tests for gloo (#1302)
    
    To resolve https://ontrack-internal.amd.com/browse/SWDEV-403530 and https://ontrack-internal.amd.com/browse/SWDEV-419837. For more context check upstream issue https://github.com/pytorch/pytorch/issues/111834
    
    Add skipIfRocmArch decorator for Navi skips (#1356)
    
    Converted NAVI check as a function (#1364)
    
    * Moved NAVI check to the test file
    
    * Revised NAVI check as a function
    
    [Navi] [Inductor] Unskip Navi inductor UTs (#1514)
    
    Relates to https://ontrack-internal.amd.com/browse/SWDEV-461590
    
    Bad import in test_torchinductor and skip torchvision related UT (#1374)
    
    skip test_inductor_freezing failing UTs (#1375)
    
    Skip test_mm_triton_kernel_benchmark (#1376)
    
    * Running triton kernel on ROCM only has one GB/s metric reported
    
    * Update test_kernel_benchmark.py
    
    skip vmapvjpvjp_linalg_householder_product_cuda_float32 (#1420)
    
    skipIfRocm needs msg parameter
    
    [NO CP] Updated changes to skip few UTs
    
    Imported skipIfRocm in certain test suites (#1577)
    
    Fixes SWDEV-472397
    
    Added functions imports (#1521)
    
    Fixes
    inductor.test_torchinductor_dynamic_shapes::TestInductorDynamicCUDA::test_item_unbacked_stride_nobreak_cuda
    
    Enable test_public_api_surface (#1601)
    
    Fixes SWDEV-462410.
    
    Enable this unit test since PyTorch issue
    https://github.com/pytorch/pytorch/issues/104012 has been closed. This
    unit test runs fine on MI100/MI300 and upstream.
    
    (cherry picked from commit 0001d4ab5070635cfecc146ee299bbb9fa45ca67)
    
    [rocm6.3_internal_testing] Fixed error string assertion in test_invalid_devices (#1607)
    
    Fixes #8974
    
    (cherry picked from commit a688e0a7e7f2b9fb092e9ebb2695d9f2caf63dc4)

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -1279,6 +1279,14 @@ IS_PPC = platform.machine() == "ppc64le"
 IS_X86 = platform.machine() in ('x86_64', 'i386')
 IS_ARM64 = platform.machine() in ('arm64', 'aarch64')
 
+def is_navi_arch():
+    if torch.cuda.is_available():
+        prop = torch.cuda.get_device_properties(0)
+        gfx_arch = prop.gcnArchName.split(":")[0]
+        if gfx_arch in ["gfx1100", "gfx1101", "gfx1102"]:
+            return True
+    return False
+
 def is_avx512_vnni_supported():
     if sys.platform != 'linux':
         return False
@@ -1756,6 +1764,19 @@ def skipIfRocm(func=None, *, msg="test doesn't currently work on the ROCm stack"
         return wrapper
     if func:
         return dec_fn(func)
+    return dec_fn
+
+def skipIfRocmArch(arch: Tuple[str, ...]):
+    def dec_fn(fn):
+        @wraps(fn)
+        def wrap_fn(self, *args, **kwargs):
+            if TEST_WITH_ROCM:
+                prop = torch.cuda.get_device_properties(0)
+                if prop.gcnArchName.split(":")[0] in arch:
+                    reason = f"skipIfRocm: test skipped on {arch}"
+                    raise unittest.SkipTest(reason)
+            return fn(self, *args, **kwargs)
+        return wrap_fn
     return dec_fn
 
 def runOnRocm(fn):
