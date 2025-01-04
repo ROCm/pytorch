cbca7453cc6084f11ae8b5854b58b0f18a8fe968
eqy <eddiey@nvidia.com>
2025-01-04 02:31:13 +0000

    [CUDA] Check `size` calculation in `ilpReduce` for `softmax` (#144009)
    For #143644
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144009
    Approved by: https://github.com/Skylion007

--- aten/src/ATen/native/cuda/SoftMax.cu
+++ aten/src/ATen/native/cuda/SoftMax.cu
@@ -468,7 +468,7 @@ ilpReduce(index_t shift,
     if(threadIdx.x >= shift){
       threadVal = r(threadVal, data[offset]);
     }
-    size -= blockDim.x;
+    size -= blockDim.x > size ? size : blockDim.x;
     data += blockDim.x;
   }
   index_t last = size % (ILP * blockDim.x);
@@ -518,7 +518,7 @@ WriteFpropResultsVectorized(
     if (threadIdx.x >= shift) {
       output[offset] = epilogue(input[offset]);
     }
-    size -= blockDim.x;
+    size -= blockDim.x > size ? size : blockDim.x;
     input += blockDim.x;
     output += blockDim.x;
   }
