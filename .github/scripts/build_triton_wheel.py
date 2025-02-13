311fe7007dbe0a1aa65fe34e70d73204540f9327
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2025-02-13 21:22:14 +0530

    Cleanup

--- .github/scripts/build_triton_wheel.py
+++ .github/scripts/build_triton_wheel.py
@@ -91,7 +91,7 @@ def build_triton(
         # Nightly binaries include the triton commit hash, i.e. 2.1.0+e6216047b8
         # while release build should only include the version, i.e. 2.1.0
         rocm_version = get_rocm_version()
-        version_suffix = f"+rocm{rocm_version}_{commit_hash[:10]}"
+        version_suffix = f"+rocm{rocm_version}.git{commit_hash[:8]}"
         version += version_suffix
     with TemporaryDirectory() as tmpdir:
         triton_basedir = Path(tmpdir) / "triton"
@@ -164,8 +164,6 @@ def build_triton(
             return Path.cwd() / conda_path.name
 
         # change built wheel name and version
-        rocm_version = get_rocm_version()
-        version_suffix = f"+rocm{rocm_version}.git{commit_hash[:8]}"
         env["TRITON_WHEEL_NAME"] = triton_pkg_name
         env["TRITON_WHEEL_VERSION_SUFFIX"] = version_suffix
         if with_clang_ldd:
