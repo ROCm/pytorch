b03b2b628e7157bedd23c3a9daaefb7ed79e0b34
Qubitium-ModelCloud <qubitium@modelcloud.ai>
2025-03-17 09:12:17 +0800

    fix should not touch flag values

--- torch/utils/cpp_extension.py
+++ torch/utils/cpp_extension.py
@@ -1055,7 +1055,16 @@ class BuildExtension(build_ext):
             modified_flags = []
             for flag in extension.extra_compile_args['nvcc']:
                 if flag.startswith("-") and "CUDA" in flag and not flag.startswith("-I"):
-                    modified_flag = flag.replace("CUDA", "HIP", 1)
+                    # check/split flag into flag and value
+                    parts = flag.split("=", 1)
+                    if len(parts) == 2:
+                        flag_part, value_part = parts
+                        # replace fist instance of "CUDA" with "HIP" only in the flag and not flag value
+                        modified_flag_part = flag_part.replace("CUDA", "HIP", 1)
+                        modified_flag = f"{modified_flag_part}={value_part}"
+                    else:
+                        # replace fist instance of "CUDA" with "HIP" in flag
+                        modified_flag = flag.replace("CUDA", "HIP", 1)
                     modified_flags.append(modified_flag)
                     print(f'Modified flag: {flag} -> {modified_flag}', file=sys.stderr)
                 else:
