f5f899297cd7459a29a10125951cfbee2ce01dda
Jithun Nair <jithun.nair@amd.com>
2025-04-10 20:28:20 +0000

    Refactor ck.cmake

--- cmake/External/ck.cmake
+++ cmake/External/ck.cmake
@@ -1,51 +1,60 @@
 #
 # create INTERFACE target for CK library
 #
-if(NOT __CK_KERNELS_INCLUDED)
-  set(__CK_KERNELS_INCLUDED TRUE)
+if(NOT __ck_kernels_included)
+  set(__ck_kernels_included TRUE)
 
-  set(CK_KERNELS_VERSION 0.1)
+  set(ck_kernels_install_dir "${PROJECT_SOURCE_DIR}/torch/lib")
 
-  # get CK commit hash
-  execute_process(
-      COMMAND git -C ${CMAKE_SOURCE_DIR}/third_party submodule status composable_kernel
-      RESULT_VARIABLE result
-      OUTPUT_VARIABLE submodule_status
-      ERROR_VARIABLE submodule_status_error
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-  if(result EQUAL 0)
-      string(REGEX REPLACE "^[ \t]" "" submodule_status ${submodule_status})
-      # extract first 8 characters of the commit hash
-      string(SUBSTRING "${submodule_status}" 0 8 ck_commit_hash)
-  else()
-      message(FATAL_ERROR "Failed to get submodule status for composable_kernel.")
-  endif()
-  
-  # full path for CK library on compute-artifactory.amd.com
-  # set(CK_KERNELS_PACKAGE_BASE_URL $ENV{CK_KERNELS_PACKAGE_BASE_URL})
-  set(CK_KERNELS_PACKAGE_BASE_URL "https://compute-artifactory.amd.com/artifactory/rocm-generic-local")
-  if (NOT CK_KERNELS_PACKAGE_BASE_URL)
-      message("CK_KERNELS_PACKAGE_BASE_URL env var is not defined. Not installing prebuilt CK_kernels.")
-      return()
-  else()
-      set(ck_lib_full_path "${CK_KERNELS_PACKAGE_BASE_URL}/torch_ck_gen_lib/ck_${ck_commit_hash}/rocm_${ROCM_VERSION_DEV}/libck_kernels.so")
-  endif() 
-  
-  # set destination
-  set(destination "${CMAKE_SOURCE_DIR}/torch/lib/libck_kernels.so")
-  
-  # download CK library
-  file(DOWNLOAD ${ck_lib_full_path} ${destination} SHOW_PROGRESS RESULT_VARIABLE download_status)
-  if(NOT download_status)
-      message(STATUS "Downloaded CK library successfully.")
-  else()
-      message(FATAL_ERROR "Failed to download the CK library from ${SOURCE_URL}.")
-  endif()
-  
+  set(__ck_kernels_version 0.1)
+
   # create INTERFACE target
   add_library(__ck_lib INTERFACE)
+
+  if(DEFINED ENV{CK_KERNELS_INSTALLED_PREFIX})
+    # Copy .so from $ENV{CK_KERNELS_INSTALLED_PREFIX} into ${ck_kernels_install_dir}
+    install(DIRECTORY
+            $ENV{CK_KERNELS_INSTALLED_PREFIX}/
+            DESTINATION ${ck_kernels_install_dir}
+            FILES_MATCHING_PATTERN "*.so")
+    message(STATUS "Using Preinstalled CK_kernels from $ENV{CK_KERNELS_INSTALLED_PREFIX}; installed at ${ck_kernels_install_dir}")
+  elseif(DEFINED ENV{CK_KERNELS_PACKAGE_BASE_URL})
+    # get CK commit hash
+    execute_process(
+        COMMAND git -C ${CMAKE_SOURCE_DIR}/third_party submodule status composable_kernel
+        RESULT_VARIABLE result
+        OUTPUT_VARIABLE submodule_status
+        ERROR_VARIABLE submodule_status_error
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    if(result EQUAL 0)
+        string(REGEX REPLACE "^[ \t]" "" submodule_status ${submodule_status})
+        # extract first 8 characters of the commit hash
+        string(SUBSTRING "${submodule_status}" 0 8 ck_commit_hash)
+    else()
+        message(FATAL_ERROR "Failed to get submodule status for composable_kernel.")
+    endif()
+
+    set(ck_kernels_package_full_url "$ENV{CK_KERNELS_PACKAGE_BASE_URL}/torch_ck_gen_lib/ck_${ck_commit_hash}/rocm_${ROCM_VERSION_DEV}/libck_kernels.tar.gz")
+    set(ck_kernels_install_path "${ck_kernels_install_dir}/libck_kernels.so")
+
+    ExternalProject_Add(ck_kernels_external
+      URL "${ck_kernels_package_full_url}"
+      # URL_HASH 
+      SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/ck_kernels_tarball
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory
+      "${CMAKE_CURRENT_BINARY_DIR}/ck_kernels_tarball"
+      "${ck_kernels_install_dir}"
+      INSTALL_BYPRODUCTS "${ck_kernels_install_path}"
+    )
+    add_dependencies(__ck_lib ck_kernels_external)
+    message(STATUS "Using CK_kernels from pre-compiled binary ${ck_kernels_package_full_url}; installed at ${ck_kernels_install_dir}")
+  else()
+    message(FATAL_ERROR "Unable to find an existing CK_kernels installation or to install CK_kernels library")
+  endif() # DEFINED ENV{AOTRITON_INSTALLED_PREFIX}
   
   # specify path to CK library
-  target_link_libraries(__ck_lib INTERFACE ${destination})
-endif() # __CK_KERNELS_INCLUDED
+  target_link_libraries(__ck_lib INTERFACE ${ck_kernels_install_path})
+endif() # __ck_kernels_included
