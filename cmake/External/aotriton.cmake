ROCm/pytorch

commit 63cbb2cc98150fec7f0f83980e661707eed69fd8

Update to AOTriton 0.9.2b with gfx950 support package (#2074)
This packages mixes the support of conventional architectures and
gfx950, and also removes the "experimental" status of gfx950 for testing

cmake/External/aotriton.cmake
@@ -22,22 +22,16 @@ if(NOT __AOTRITON_INCLUDED)
   # Replaces .ci/docker/aotriton_version.txt
   # Note packages information may have versions skipped (due to no ABI breaks)
   # But they must be listed from lower version to higher version
-  set(__AOTRITON_VER "0.9.2b")
+  set(__AOTRITON_VER "0.9.2b_612896439f")
   set(__AOTRITON_MANYLINUX_LIST
-      "manylinux_2_28"  # rocm6.2
-      "manylinux_2_28"  # rocm6.3
-      "manylinux_2_28"  # rocm6.4
+      "manylinux_2_28"  # rocm6.5
       )
   set(__AOTRITON_ROCM_LIST
-      "rocm6.2"
-      "rocm6.3"
-      "rocm6.4"
+      "rocm6.5"
       )
-  set(__AOTRITON_CI_COMMIT "b388d223d8c7213545603e00f6f3148c54d1f525")
+  set(__AOTRITON_CI_COMMIT "612896439fb4f78509b1a566b5ef0a333e9585bb")
   set(__AOTRITON_SHA256_LIST
-      "08d84f96f4c984179f80f517c0431c7511ee26bb0ce9bd05a827573ddd78cc79"  # rocm6.2
-      "9094d59717e7e6eace9126ca100dd0e86510f07fc6c3a349569fc4e2d9056604"  # rocm6.3
-      "41190202c2736d5ff75b13a3abc0fb52ebfbb67226cf85dc3de7699c7000db44"  # rocm6.4
+      "c85da64d21510190277794455ef8bd3f2d543a6f2462140d3da27e1df0ab8f82"  # rocm6.5
       )
   set(__AOTRITON_Z "gz")
 

commit 423f0b73b666f23edaa1970c762423f572f53b5b

Introduce __AOTRITON_VER_WITH_COMMIT
To resolve build issues introduced by https://github.com/ROCm/pytorch/pull/2074, since the actual url - `https://github.com/ROCm/aotriton/releases/download/0.9.2b/aotriton-0.9.2b_612896439f-manylinux_2_28_x86_64-rocm6.5-shared.tar.gz` -  doesn't match the expected value: `/ROCm/aotriton/releases/download/0.9.2b_612896439f/aotriton-0.9.2b_612896439f-manylinux_2_28_x86_64-rocm6.5-shared.tar.gz`

cmake/External/aotriton.cmake
@@ -22,7 +22,8 @@ if(NOT __AOTRITON_INCLUDED)
   # Replaces .ci/docker/aotriton_version.txt
   # Note packages information may have versions skipped (due to no ABI breaks)
   # But they must be listed from lower version to higher version
-  set(__AOTRITON_VER "0.9.2b_612896439f")
+  set(__AOTRITON_VER "0.9.2b")
+  set(__AOTRITON_VER_WITH_COMMIT "0.9.2b_612896439f")
   set(__AOTRITON_MANYLINUX_LIST
       "manylinux_2_28"  # rocm6.5
       )
@@ -89,7 +90,7 @@ if(NOT __AOTRITON_INCLUDED)
     list(GET __AOTRITON_MANYLINUX_LIST ${__AOTRITON_ROCM_INDEX} __AOTRITON_MANYLINUX)
     set(__AOTRITON_ARCH ${CMAKE_HOST_SYSTEM_PROCESSOR})
     string(CONCAT __AOTRITON_FILE "aotriton-"
-                                  "${__AOTRITON_VER}-${__AOTRITON_MANYLINUX}"
+                                  "${__AOTRITON_VER_WITH_COMMIT}-${__AOTRITON_MANYLINUX}"
                                   "_${__AOTRITON_ARCH}-rocm${__AOTRITON_ROCM}"
                                   "-shared.tar.${__AOTRITON_Z}")
     string(CONCAT __AOTRITON_URL "https://github.com/ROCm/aotriton/releases/download/"