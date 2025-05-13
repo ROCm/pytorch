b6507161b62783c25f79bd8196f469d4ea27fde3
iupaikov-amd <Iurii.Paikov@amd.com>
2025-05-13 18:52:32 +0200

    Cherry-picked commit with merge conflict

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -109,7 +109,20 @@ except ImportError:
 
 MI300_ARCH = ("gfx940", "gfx941", "gfx942")
 NAVI_ARCH = ("gfx1030", "gfx1100", "gfx1101", "gfx1200", "gfx1201")
+<<<<<<< HEAD
 
+=======
+NAVI3_ARCH = ("gfx1100", "gfx1101")
+NAVI4_ARCH = ("gfx1200", "gfx1201")
+>>>>>>> 4ce2c1490e ([release/2.5] test_decompose_mem_bound_mm.py tolerance increase for navi3x (#2129))
+
+def is_navi3_arch():
+    if torch.cuda.is_available():
+        prop = torch.cuda.get_device_properties(0)
+        gfx_arch = prop.gcnArchName.split(":")[0]
+        if gfx_arch in NAVI3_ARCH:
+            return True
+    return False
 
 def freeze_rng_state(*args, **kwargs):
     return torch.testing._utils.freeze_rng_state(*args, **kwargs)
