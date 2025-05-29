4f116b08e19f1e264e9aac42386704fa6135a53a
Arash Pakbin <Arash.Pakbin@amd.com>
2025-05-29 21:10:45 +0000

    [ROCm] Exposing Some MIOpen Symbols (#2176) (#154545)
    This PR exposes some MIOpen symbols, namely:
    
    1. `miopenDataType_t getMiopenDataType(const at::Tensor& tensor)`
    2. `miopenHandle_t getMiopenHandle()`
    3. `class TensorDescriptor`
    4. `class Descriptor`
    5. `class FilterDescriptor`
    6. `struct ConvolutionDescriptor`
    7. `struct DropoutDescriptor`
    8. `struct RNNDescriptor`
    
    to enable adding extensions that make use of them.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/154545
    Approved by: https://github.com/jeffdaily, https://github.com/Skylion007
    
    Co-authored-by: Jeff Daily <jeff.daily@amd.com>

--- aten/src/ATen/miopen/Handle.h
+++ aten/src/ATen/miopen/Handle.h
@@ -3,8 +3,9 @@
 #include <ATen/miopen/miopen-wrapper.h>
 #include <c10/macros/Export.h>
 
-namespace at { namespace native {
+namespace at::native {
 
 TORCH_CUDA_CPP_API miopenHandle_t getMiopenHandle();
+<<<<<<< HEAD
 
-}} // namespace
+} // namespace at::native
