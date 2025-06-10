eed6ad303ba0f631466f3cbc66b7865b586bfe0d
Akash Verma <Akash.Verma3@amd.com>
2025-06-10 17:17:43 +0000

    Using requires_multicast_support to skip the test rather than skipIfRocm.

--- test/distributed/test_symmetric_memory.py
+++ test/distributed/test_symmetric_memory.py
@@ -1012,7 +1012,7 @@ class SymmMemCollectiveTest(MultiProcessTestCase):
                 atol=1e-01,
             )
 
-    @skipIfRocm
+    @requires_multicast_support()
     @skip_if_lt_x_gpu(4)
     @parametrize("align_bytes", [4, 8, 16])
     def test_multimem_all_gather(self, align_bytes: int) -> None:
