a544d27733c9c46b9516e14f5871d13b9c254bb8
Jack Taylor <jack.taylor@amd.com>
2025-08-10 19:05:19 -0500

    Add fast tanh

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -1288,7 +1288,7 @@ class TritonOverrides(OpOverrides):
     @staticmethod
     @maybe_upcast_float32()
     def tanh(x):
-        return f"libdevice.tanh({x})"
+        return f"libdevice.fast_tanhf({x})"
 
     @staticmethod
     @maybe_upcast_float32()
