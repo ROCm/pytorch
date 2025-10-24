39aaf5309b75b09e64b6a6189ed2b2026ac99fab
Slobodan Josic <Slobodan.Josic@amd.com>
2025-10-24 11:19:44 +0200

    fix: hipBLASLt support for gfx15X has been added starting from ROCm 6.4.2

--- aten/src/ATen/Context.cpp
+++ aten/src/ATen/Context.cpp
@@ -336,7 +336,10 @@ at::BlasBackend Context::blasPreferredBackend() {
       static const std::vector<std::string> archs = {
           "gfx90a", "gfx942",
 #if ROCM_VERSION >= 60400
-          "gfx1150", "gfx1151", "gfx1200", "gfx1201",
+          "gfx1200", "gfx1201",
+#endif
+#if ROCM_VERSION >= 60402
+          "gfx1150", "gfx1151",
 #endif
 #if ROCM_VERSION >= 60500
           "gfx950"
@@ -363,6 +366,9 @@ at::BlasBackend Context::blasPreferredBackend() {
           "gfx90a", "gfx942",
 #if ROCM_VERSION >= 60300
           "gfx1100", "gfx1101", "gfx1200", "gfx1201",
+#endif
+#if ROCM_VERSION >= 60402
+          "gfx1150", "gfx1151",
 #endif
 #if ROCM_VERSION >= 60500
           "gfx950"
