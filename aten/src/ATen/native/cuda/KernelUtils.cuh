e0609a687408a1fa54ab2e817c65637b9d0f14fb
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2025-11-17 00:01:17 -0800

    Use s_wait_loadcnt for cmtdStore

--- aten/src/ATen/native/cuda/KernelUtils.cuh
+++ aten/src/ATen/native/cuda/KernelUtils.cuh
@@ -220,7 +220,7 @@ __device__ __forceinline__ void fastAtomicAdd(
 }
 
 
-#if (defined(USE_ROCM) && (defined(__gfx940__) || defined(__gfx941__) || defined(__gfx942__) || defined(__gfx950__)))
+#ifdef USE_ROCM
 // This function implements a committed store.
 // Upon returning, the store is committed to global memory.
 // This is useful in avoiding the need for fences.
