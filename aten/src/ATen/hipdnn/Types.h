3abbee2cc9474065b8e93bb08d32c61a3084c26e
Dmitry Nikolaev <dmitry.nikolaev@amd.com>
2026-02-20 18:57:17 +0000

    batchnorm

---
+++ aten/src/ATen/hipdnn/Types.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <ATen/Tensor.h>
+#include <ATen/hipdnn/hipdnn-wrapper.h>
+#include <c10/macros/Export.h>
+
+namespace at::native {
+
+TORCH_HIP_CPP_API hipdnn_frontend::DataType getHipdnnDataType(const at::Tensor& tensor);
+
+// int64_t miopen_version();
+
+} // namespace at::native
