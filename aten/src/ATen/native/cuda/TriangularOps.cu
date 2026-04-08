5f57f4223c43fad8dd9a6ae29409ea10ee61c7c8
Dmitry Nikolaev <dmitry.nikolaev@amd.com>
2026-04-08 00:48:29 +0000

    smart-3

--- aten/src/ATen/native/cuda/TriangularOps.cu
+++ aten/src/ATen/native/cuda/TriangularOps.cu
@@ -139,7 +139,7 @@ void triu_tril_cuda_template(const Tensor& result, const Tensor& self, int64_t k
     // Tune elements_per_thread for optimal performance on MI300X
     constexpr int elements_per_thread = 
       sizeof(scalar_t) <= 2 ? 4 :    // 4 elements per thread for 16-bit and 8-bit scalars
-      sizeof(scalar_t) == 4 ? 2 : 1; // 2 elements per thread for 32-bit scalars and 1 for larger
+      sizeof(scalar_t) <= 8 ? 2 : 1; // 2 elements per thread for 32-bit scalars and 1 for larger
 #endif // !defined(USE_ROCM)
     auto sizes = self.sizes();
     int64_t last_dim_padded = round_up<int64_t>(sizes.back(), elements_per_thread);
