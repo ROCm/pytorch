938202ebbf0568d5a7f61154cbad475666a30ace
Michael Wootton <michael.wootton@amd.com>
2026-05-05 01:44:12 -0500

    Cleanup and polish.  Add rlog property support.
    (cherry picked from commit cb63ec6606b2ef8c1b10ac42603c04bc964a4218)

--- torch/csrc/profiler/standalone/rlog_client.cpp
+++ torch/csrc/profiler/standalone/rlog_client.cpp
@@ -6,6 +6,14 @@
 #include <torch/csrc/profiler/util.h>
 #include <nlohmann/json.hpp>
 
+#include <algorithm>
+#include <atomic>
+#include <cctype>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+#include <utility>
+
 
 using at::RecordFunction;
 using at::RecordFunctionCallback;
@@ -14,12 +22,12 @@ using at::ObserverContext;
 
 namespace {
 
-  RecordFunctionCallback *cb = NULL;
-  CallbackHandle handle = 0;
+  std::unique_ptr<RecordFunctionCallback> cb;
+  std::atomic<CallbackHandle> handle {0};
 
   // Rlog interface
 
-  bool isLogging {false};
+  std::atomic<bool> isLogging {false};
   void rlog_callback_function() {
     isLogging = rlog::isActive();
     if (handle != 0) {
@@ -39,10 +47,17 @@ namespace {
       rlog::setDefaultCategory("");
     }
   };
-  Client client;
   //----------------
 
 const char* categories[(uint8_t)at::RecordScope::NUM_SCOPES] = { "" };
+
+std::atomic<bool> record_shapes {false};
+std::atomic<bool> record_input_op_ids {false};
+std::once_flag init_flag;
+std::mutex producer_map_mutex;
+// Keyed by TensorImpl* cast to void*. No invalidation on tensor destruction; reused addresses
+// overwrite stale entries. This matches the NVTX implementation and is an accepted limitation.
+std::unordered_map<void*, std::pair<at::RecordFunctionHandle, int>> producer_tensor_map;
 
 
 std::unique_ptr<ObserverContext> enter_callback(const RecordFunction &func)
@@ -53,7 +68,29 @@ std::unique_ptr<ObserverContext> enter_callback(const RecordFunction &func)
     //object["scope"] = categories[(uint8_t)func.scope()];
     object["seq"] = func.seqNr();
     object["op_id"] = func.handle();
-    object["sizes"] = torch::profiler::impl::inputSizes(func, true);
+    if (record_shapes)
+      object["sizes"] = torch::profiler::impl::inputSizes(func, true);
+    if (record_input_op_ids) {
+      nlohmann::json op_ids = nlohmann::json::array();
+      std::lock_guard<std::mutex> lock(producer_map_mutex);
+      for (const c10::IValue& input : func.inputs()) {
+        if (input.isTensor()) {
+          const at::Tensor& tensor = input.toTensor();
+          if (tensor.defined()) {
+            auto it = producer_tensor_map.find((void*)tensor.unsafeGetTensorImpl());
+            if (it != producer_tensor_map.end())
+              op_ids.push_back({it->second.first, it->second.second});
+            else
+              op_ids.push_back(nullptr);
+          } else {
+            op_ids.push_back(nullptr);
+          }
+        } else {
+          op_ids.push_back(nullptr);
+        }
+      }
+      object["input_op_ids"] = op_ids;
+    }
     rlog::rangePush(categories[(uint8_t)func.scope()], func.name(), object.dump().c_str());
   }
   return NULL;
@@ -62,6 +99,18 @@ std::unique_ptr<ObserverContext> enter_callback(const RecordFunction &func)
 void exit_callback(const RecordFunction &func, ObserverContext *context)
 {
   if (isLogging) {
+    if (record_input_op_ids) {
+      std::lock_guard<std::mutex> lock(producer_map_mutex);
+      int output_nr = 0;
+      for (const c10::IValue& output : func.outputs()) {
+        if (output.isTensor()) {
+          const at::Tensor& tensor = output.toTensor();
+          if (tensor.defined())
+            producer_tensor_map[(void*)tensor.unsafeGetTensorImpl()] = {func.handle(), output_nr};
+        }
+        output_nr++;
+      }
+    }
     rlog::rangePop();
   }
 }
@@ -72,26 +121,40 @@ void exit_callback(const RecordFunction &func, ObserverContext *context)
 namespace torch {
 
 void global_rlog_init() {
-  categories[(uint8_t)at::RecordScope::FUNCTION] = "function";
-  categories[(uint8_t)at::RecordScope::BACKWARD_FUNCTION] = "backward_function";
-  categories[(uint8_t)at::RecordScope::TORCHSCRIPT_FUNCTION] = "torchscript_function";
-  categories[(uint8_t)at::RecordScope::KERNEL_FUNCTION_DTYPE] = "kernel_function_dtype";
-  categories[(uint8_t)at::RecordScope::CUSTOM_CLASS] = "custom_class";
-  categories[(uint8_t)at::RecordScope::BUILD_FEATURE] = "build_feature";
-  categories[(uint8_t)at::RecordScope::LITE_INTERPRETER] = "lite_interpreter";
-  categories[(uint8_t)at::RecordScope::USER_SCOPE] = "user_scope";
-  categories[(uint8_t)at::RecordScope::STATIC_RUNTIME_OP] = "static_runtime_op";
-  categories[(uint8_t)at::RecordScope::STATIC_RUNTIME_MODEL] = "static_runtime_model";
+  std::call_once(init_flag, []() {
+    static Client client;
+    categories[(uint8_t)at::RecordScope::FUNCTION] = "function";
+    categories[(uint8_t)at::RecordScope::BACKWARD_FUNCTION] = "backward_function";
+    categories[(uint8_t)at::RecordScope::TORCHSCRIPT_FUNCTION] = "torchscript_function";
+    categories[(uint8_t)at::RecordScope::KERNEL_FUNCTION_DTYPE] = "kernel_function_dtype";
+    categories[(uint8_t)at::RecordScope::CUSTOM_CLASS] = "custom_class";
+    categories[(uint8_t)at::RecordScope::BUILD_FEATURE] = "build_feature";
+    categories[(uint8_t)at::RecordScope::LITE_INTERPRETER] = "lite_interpreter";
+    categories[(uint8_t)at::RecordScope::USER_SCOPE] = "user_scope";
+    categories[(uint8_t)at::RecordScope::STATIC_RUNTIME_OP] = "static_runtime_op";
+    categories[(uint8_t)at::RecordScope::STATIC_RUNTIME_MODEL] = "static_runtime_model";
 
-  cb = new RecordFunctionCallback(enter_callback, exit_callback);
-  cb->needsInputs(true);
-  //cb->needsOutputs(true);
-  cb->needsIds(true);
+    auto propBool = [](const char* property, const char* defaultValue) {
+      std::string val = rlog::getProperty("torch", property, defaultValue);
+      std::transform(val.begin(), val.end(), val.begin(),
+                     [](unsigned char c) { return std::tolower(c); });
+      if (val == "true")  return true;
+      if (val == "false") return false;
+      try { return std::stoi(val) != 0; } catch (...) { return false; }
+    };
 
-  // Register Callback and disable
-  handle = at::addThreadLocalCallback(*cb);
-  if (isLogging == false)
-      at::disableCallback(handle);
+    record_shapes = propBool("record_shapes", "true");
+    record_input_op_ids = propBool("record_input_op_ids", "false");
+    cb = std::make_unique<RecordFunctionCallback>(enter_callback, exit_callback);
+    cb->needsInputs(record_shapes || record_input_op_ids);
+    cb->needsOutputs(record_input_op_ids);
+    cb->needsIds(true);
+
+    // Register Callback and disable
+    handle = at::addGlobalCallback(*cb);
+    if (isLogging == false)
+        at::disableCallback(handle);
+  });
 }
 
 } // namespace torch
