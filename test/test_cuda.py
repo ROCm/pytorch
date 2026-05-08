56e8ae7c512bcd4b45a0e48d049ae77453f0473e
iupaikov-amd <iurii.paikov@amd.com>
2026-05-08 13:56:15 +0000

    Applied linter suggestions

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -1064,8 +1064,7 @@ print("done")
 """
         proc = subprocess.run(
             [sys.executable, "-c", code],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            capture_output=True,
             text=True,
             timeout=20,
         )
