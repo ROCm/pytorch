926b3d802bd3467c4a5b25aee2794bada348659f
iupaikov-amd <iurii.paikov@amd.com>
2026-05-08 13:56:15 +0000

    Applied linter suggestions
    (cherry picked from commit ba9672d24b7011022065bcea3696230f01932409)

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -1366,8 +1366,7 @@ print("done")
 """
         proc = subprocess.run(
             [sys.executable, "-c", code],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            capture_output=True,
             text=True,
             timeout=20,
         )
