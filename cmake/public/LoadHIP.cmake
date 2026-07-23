edfe0fbda33cfaf3a32ae10febae7d083eb5e701
Zhuojin Liu <zhuojin.liu@amd.com>
2026-07-23 13:23:57 -0400

    [release/2.10] rocm-smi deprecation (#3436)
    Validation:
    this branch: https://github.com/ROCm/TheRock/actions/runs/29505795730
    same rocm nightly on 2.10:
    https://github.com/ROCm/rockrel/actions/runs/28492200755
    
    ---------
    
    Co-authored-by: Prachi Gupta <prachi.gupta@amd.com>

--- cmake/public/LoadHIP.cmake
+++ cmake/public/LoadHIP.cmake
@@ -194,7 +194,6 @@ endif()
   if(UNIX)
     find_package_and_print_version(rccl)
     find_package_and_print_version(hsa-runtime64 REQUIRED)
-    find_package_and_print_version(rocm_smi REQUIRED)
   endif()
 
   # Optional components.
